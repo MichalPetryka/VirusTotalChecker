534dea0a3f19cf7f55ee8894c4bb0b3cf64a1fa9
petris <paprikapolishgamer@gmail.com>
2020-07-16 13:40:11 +0200

    Add encryption code template

---
+++ VirusTotalChecker.Tests/PasswordHelpersTests.cs
@@ -0,0 +1,24 @@
+﻿using VirusTotalChecker.Utilities;
+using Xunit;
+
+namespace VirusTotalChecker.Tests
+{
+	public class PasswordHelpersTests
+	{
+		[Theory]
+		[InlineData("", "qwert test ąć@$%")]
+		[InlineData("qwert test ąć@$%", "qwert test ąć@$%")]
+		public void EncryptionTest(string text, string key)
+		{
+			Assert.Equal(text, PasswordHelpers.DecryptString(PasswordHelpers.EncryptString(text, key), key));
+		}
+
+		[Theory]
+		[InlineData("", "CF83E1357EEFB8BDF1542850D66D8007D620E4050B5715DC83F4A921D36CE9CE47D0D13C5D85F2B0FF8318D2877EEC2F63B931BD47417A81A538327AF927DA3E")]
+		[InlineData("qwert test ąć@$%", "D03BAB1FBAC0CCEDEA1CE5DA13BF9428D02D1F24621FD5F32C1174C8E21DD6E7E688D2589E3AB426D7C65F466C0633D6535A2B75F9F019BC066ADB00F9143CFA")]
+		public void Sha512Test(string text, string hash)
+		{
+			Assert.Equal(hash, PasswordHelpers.GetSha512(text));
+		}
+	}
+}
---
+++ VirusTotalChecker/Utilities/PasswordHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VirusTotalChecker.Utilities
+{
+	public static class PasswordHelpers
+	{
+		private static readonly UTF8Encoding Encoding = new UTF8Encoding(false);
+
+		public static string EncryptString(string plainText, string key)
+		{
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = Encoding.GetBytes(key);
+				byte[] array = ArrayPool<byte>.Shared.Rent(Encoding.GetMaxByteCount(plainText.Length));
+				int length = Encoding.GetBytes(plainText, array);
+				byte[] encrypted;
+				using (ICryptoTransform transform = aes.CreateEncryptor())
+					encrypted = transform.TransformFinalBlock(array, 0, length);
+				ArrayPool<byte>.Shared.Return(array);
+				return Convert.ToBase64String(encrypted);
+			}
+		}
+
+		public static string DecryptString(string cipherText, string key)
+		{
+			byte[] buffer = Convert.FromBase64String(cipherText);
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = Encoding.GetBytes(key);
+				using (ICryptoTransform transform = aes.CreateDecryptor())
+					return Encoding.GetString(transform.TransformFinalBlock(buffer, 0, buffer.Length));
+			}
+		}
+
+		public static string GetSha512(string text)
+		{
+			using (SHA512 sha = SHA512.Create())
+				return sha.ComputeHash(Encoding.GetBytes(text)).ToHexString();
+		}
+	}
+}
